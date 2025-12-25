075200feb1a23c00e920dae583596e68664a6ac9
Maciej <maciej.polowy1@gmail.com>
2025-12-25 12:15:45 +0100

    fix

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -6,8 +6,6 @@ export const metadata: Metadata = {
 };
 
 export default function LoginPage() {
-  throw new Error("xd");
-
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
